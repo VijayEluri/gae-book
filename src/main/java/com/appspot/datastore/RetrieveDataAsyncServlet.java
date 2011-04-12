8301517d729ac66bd664fd11020bf7c97a2732d0
Adriaan de Jonge <adriaandejonge@gmail.com>
2011-04-12 22:10:01 +0200

    Add asynchronous calls

---
+++ src/main/java/com/appspot/datastore/RetrieveDataAsyncServlet.java
@@ -0,0 +1,46 @@
+package com.appspot.datastore;
+
+import com.google.appengine.api.datastore.*;
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class RetrieveDataAsyncServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getRequestURI().replaceAll("/retrieve-data/", "");
+        AsyncDatastoreService datastoreService = DatastoreServiceFactory
+                     .getAsyncDatastoreService();
+
+        // start asynchronous call
+        Future<Entity> blogPostFuture = datastoreService.get(KeyFactory.createKey("BlogPost", id));
+
+        // do something else
+
+        Entity blogPost = null;
+        try {
+            // and block until result is available
+            blogPost = blogPostFuture.get();
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        }
+
+
+        StringTemplateGroup group = new StringTemplateGroup("xhtml",
+                "WEB-INF/templates/xhtml");
+        StringTemplate html = group.getInstanceOf("retrieve-blog-post");
+        html.setAttributes(blogPost.getProperties());
+
+        response.getWriter().write(html.toString());
+    }
+}
