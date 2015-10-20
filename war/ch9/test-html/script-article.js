ad80f6f4c98a15cfdd27961f52482c602aa2138a
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-10-19 22:11:11 -0700

    Remove debug info

--- war/ch9/test-html/script-article.js
+++ war/ch9/test-html/script-article.js
@@ -4,10 +4,7 @@ var myModule = Object.create({
 ajaxHandler : function (e) {
  if(this.readyState == 4 && this.status == 200) {
    var selected = document.querySelectorAll('article');
-   window.alert('1' + selected.item(0).innerHTML);
-   
    selected.item(0).innerHTML = this.responseText;
-    window.alert('1' + selected.item(0).innerHTML);
  } else if (this.readyState == 4 && this.status != 200) {
    // error handling
  } else {
